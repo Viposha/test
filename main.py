08f7e08e554111b3eecfc0b3fb3615c4e5811084
Viposha <Viposha@gmail.com>
2022-04-12 00:37:40 +0300

    DONE now import only the function that is being used from module sys.getsizeof -> getsizeof

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 def main():
-    import sys  # TODO now import only the function that is being used from module
+    from sys import getsizeof  # DONE now import only the function that is being used from module
     import time
     # start change
     start = time.time()
 
     test_set = range(100000)
-    print(f' The length of set is {len(set)} numbers')
+    print(f' The length of set is {len(test_set)} numbers')
 
-    test_list = list(set)
-    test_tuple = tuple(set)
+    test_list = list(test_set)
+    test_tuple = tuple(test_set)
     # TODO: call test_list and test_tuple funcs several times
 
     print(type(test_list))
     print(type(test_tuple))
 
-    print(f'Size of test_list is {sys.getsizeof(test_list)} bytes')
-    print(f'Size of test_tuple is {sys.getsizeof(test_tuple)} bytes')
+    print(f'Size of test_list is {getsizeof(test_list)} bytes')
+    print(f'Size of test_tuple is {getsizeof(test_tuple)} bytes')
     # TODO reformat code to fit only 89 positions per line
     print(
-        f'The difference in memory between test_tuple and test_list is {sys.getsizeof(test_list) - sys.getsizeof(test_tuple)} bytes')
+        f'The difference in memory between test_tuple and test_list is {getsizeof(test_list) - getsizeof(test_tuple)} bytes')
 
     end = time.time()
     print(f'Runtime is {end - start} sec')
