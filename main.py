fbb80de72c29c738d19f025da3f515e30a6d34fa
Viposha <Viposha@gmail.com>
2022-04-12 00:08:40 +0300

    change set to test_set

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def main():
-    import sys # TODO now import only the function that is being used from module
+    import sys  # TODO now import only the function that is being used from module
     import time
 
     start = time.time()
 
-    set = range(100000)
+    test_set = range(100000)
     print(f' The length of set is {len(set)} numbers')
 
     test_list = list(set)
@@ -17,12 +17,11 @@ def main():
     print(f'Size of test_list is {sys.getsizeof(test_list)} bytes')
     print(f'Size of test_tuple is {sys.getsizeof(test_tuple)} bytes')
     # TODO reformat code to fit only 89 positions per line
-    print(f'The difference in memory between test_tuple and test_list is {sys.getsizeof(test_list)-sys.getsizeof(test_tuple)} bytes')
+    print(
+        f'The difference in memory between test_tuple and test_list is {sys.getsizeof(test_list) - sys.getsizeof(test_tuple)} bytes')
 
     end = time.time()
-    print(f'Runtime is {end-start} sec')
-
-
+    print(f'Runtime is {end - start} sec')
 
 
 if __name__ == '__main__':
